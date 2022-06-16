dd456ba1a01d73f4927a3fd6b392b6f70a30b952
Remenyr <puertaalejo02@gmail.com>
2022-06-16 11:45:00 -0500

    foor loop

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char let;
+
+	for (let = 'a'; let <= 'z'; let++)
+		putchar(let);
+	for (let = 'A'; let <= 'Z'; let++)
+		putchar(let);
+		putchar('\n');
+	return (0);
+}
