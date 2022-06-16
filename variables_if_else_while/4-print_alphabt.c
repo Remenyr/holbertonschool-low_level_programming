a12e5e2ddf19e4395c2bfd5822185db9cf75e2c8
Remenyr <puertaalejo02@gmail.com>
2022-06-16 12:38:09 -0500

    for + if

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char let;
+
+	for (let = 'a'; let < 'z'; let++)
+{
+	if (let != 'e' && let != 'q')
+	putchar(let);
+}
+	putchar('\n');
+}
