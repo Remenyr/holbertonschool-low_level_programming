e8aa475b5b96ac3fb5ea619edaa6be360e9c1af9
Remenyr <puertaalejo02@gmail.com>
2022-06-19 19:57:15 -0500

    _putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,19 +1,22 @@
-#include <unistd.h>
-#include "holberton.h"
+#include "main.h"
 
 /**
- * main - Check description
- * Description: It prints the word Holberton, followed by a new line.
- * Return: 0.
+ *main - print a message
+ *Return: 0
  */
+
 int main(void)
 {
-	char let[8] = "_putchar";
-	int i;
-
-	for (i = 0; i < 8; i++)
-	_putchar(let[i]);
+	_putchar(95);
+	_putchar(112);
+	_putchar(117);
+	_putchar(116);
+	_putchar(99);
+	_putchar(104);
+	_putchar(97);
+	_putchar(114);
 	_putchar('\n');
 
 	return (0);
+
 }
