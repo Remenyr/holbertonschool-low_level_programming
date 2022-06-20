ae9c85340d9d76a6dad0780ed622c46b5478cd04
Remenyr <puertaalejo02@gmail.com>
2022-06-19 21:28:43 -0500

    lower o upper

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *_islower - say that is lower o upper
+ *@c: is a variable
+ *Return: 0
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+
+}
