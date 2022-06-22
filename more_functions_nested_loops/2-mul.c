f946093782f319d5f0464720f7ae19af79dc10d4
Remenyr <puertaalejo02@gmail.com>
2022-06-22 11:50:23 -0500

    product

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - check of the proyect
+ * @a: is a Variabel
+ * @b: is another variable
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+	int pro;
+	
+	pro = a * b;
+	return (pro);
+}
