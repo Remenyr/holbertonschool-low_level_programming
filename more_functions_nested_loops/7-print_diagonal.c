34b5cd1270c0edcc76d298deef5c4a2a94c72922
Remenyr <puertaalejo02@gmail.com>
2022-06-28 17:57:12 -0500

    barras

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagonal - Check of the proyect
+ * @n: is a variable
+ * Return: 0
+ */
+
+void print_diagonal(int n)
+{
+	int cant;
+       int vec;
+
+	for (cant = 1; cant <= n; cant++)
+	{
+		for (vec = 1; vec < cant; vec++)
+			putchar(' ');
+		putchar('\\');
+		putchar('\n');
+	}
+	if (n < 1)
+		putchar('\n');
+}
