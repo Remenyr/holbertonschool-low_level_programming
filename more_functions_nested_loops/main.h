35e68d63910803fa1cf0ef2096734f7332792fcf
Remenyr <puertaalejo02@gmail.com>
2022-06-29 18:40:28 -0500

    all

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -13,6 +13,7 @@ void print_square(int size);
 void print_triangle(int size);
 void print_alphabet(void);
 void print_numbers(void);
-void print_square(int size);
+void print_square(int size)
+void print_number(int n);
 
 #endif
