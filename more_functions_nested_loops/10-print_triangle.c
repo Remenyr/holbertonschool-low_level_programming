819f4b2734a9a9fea464a9e647c16e45880ab7da
Remenyr <puertaalejo02@gmail.com>
2022-06-29 16:33:46 -0500

    all

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_triangle - Check of the proyect
+ * @size: is a variable
+ *
+ * Return: 0
+ */
+
+void print_triangle(int size)
+{
+	int x, y;
+
+	for (x = 1; x <= size; x++)
+	{
+		for (y = 1; y <= size; y++)
+		{
+			if (y <= size - x)
+				putchar(' ');
+			else
+				putchar('#');
+		}
+		putchar('\n');
+	}
+}
---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check of the proyect
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+	for (n = 1; n < 100; n++)
+	{
+		if (n % 3 == 0 && n % 5 == 0)
+			printf("FizzBuzz ");
+		else if (n % 3 == 0)
+			printf("fizz ");
+		else if (n % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", n);
+	}
+	printf("Buzz\n");
+	return (0);
+}
