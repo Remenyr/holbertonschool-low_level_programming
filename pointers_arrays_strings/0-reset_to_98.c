2631e21d9eda55637e685a290ba497ebd41919e9
Remenyr <puertaalejo02@gmail.com>
2022-06-29 19:32:13 -0500

    value update

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - Check of the proyect
+ * @n: is a variable that being update
+ * Return 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
