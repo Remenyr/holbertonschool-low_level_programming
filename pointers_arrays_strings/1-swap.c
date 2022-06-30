126e7d045ce25af8f897224e5f3649c082208e97
Remenyr <puertaalejo02@gmail.com>
2022-06-29 19:48:21 -0500

    swap

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int - Check of the proyect
+ * @a: is a variable
+ * @b: is another variable
+ * Return: 0
+ */
+void swap_int(int* a, int* b) 
+{
+	*a = 42;
+	*b = 98;
+}
