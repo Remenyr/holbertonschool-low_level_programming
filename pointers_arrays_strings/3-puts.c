17e46f9fddf0bb15136932a0d59fb63f77c4f0f6
Remenyr <puertaalejo02@gmail.com>
2022-06-29 20:46:55 -0500

    cout of letters

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - check of the proyect
+ * @str: is a variable
+ * Return: 0
+ */
+
+void _puts(char *str) 
+{
+	int i;
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+	putchar(str[i]);
+	i++;
+	}
+	putchar('\n');
+}
