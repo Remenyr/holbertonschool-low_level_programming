d98431355ded02ca3ee80f88d4b36088f48a82b1
Remenyr <puertaalejo02@gmail.com>
2022-07-03 11:50:05 -0500

    all

--- recursion/101-wildcmp.c
+++ recursion/101-wildcmp.c
@@ -8,12 +8,12 @@
 int wildcmp(char *a, char *b)
 {
 	if (!*a && !*b)
-	return (1);
+		return (1);
 	if (*a == *b)
-	return (wildcmp(a + 1, b + 1));
+		return (wildcmp(a + 1, b + 1));
 	if (*b == '*' && (wildcmp(a, b + 1) || wildcmp(a + 1, b)))
-	return (1);
+		return (1);
 	if (*a == '*' && *(a + 1) && *b)
-	return (0);
+		return (0);
 	return (0);
 }
