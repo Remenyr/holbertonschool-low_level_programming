5c451ba702f9220ce86f6695d529345d752a1b6b
Remenyr <puertaalejo02@gmail.com>
2022-07-03 11:45:34 -0500

    all

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - Check of the proyect
+ * @x: is a variable
+ * @y: is a variable
+ * Return: 0
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+	return (1);
+	else if (y < 0)
+	return (-1);
+	else if (y == 1)
+	return (x);
+	return (x *= _pow_recursion(x, y - 1));
+}
