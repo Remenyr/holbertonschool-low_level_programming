9d36159fb8d6d370e8fadf0c92b6e998c0c5eb6a
Remenyr <puertaalejo02@gmail.com>
2022-07-18 11:04:13 -0500

    all

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strcpy - a function that copies the string
+ * @dest: string
+ * @src: pointer
+ * Return: 0
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	char *aux = dest;
+
+	while (*src)
+		*dest++ = *src++;
+	return (aux);
+}
