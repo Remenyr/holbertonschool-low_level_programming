1d197d96f2f20e220f042040535b2f20b3c49009
Remenyr <puertaalejo02@gmail.com>
2022-07-18 12:54:25 -0500

    all

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,22 +1,26 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * _strcat - Concatenate strings
- * @a: variable
- * @b: variable
- * Return: 0
+ *_strcat - check the code for Holberton School students.
+ *@dest: destination
+ *@src: source
+ * Return: pointer to the destination
  */
-
-char *_strcat(char *a, char *b)
+char *_strcat(char *dest, char *src)
 {
-	char *c = a;
+	int i, j;
 
-	while (*a)
-		a++;
-
-	while (*b)
-		*a++ = *b++;
-
-	*a = '\0';
-	return (c);
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	return (dest);
 }
