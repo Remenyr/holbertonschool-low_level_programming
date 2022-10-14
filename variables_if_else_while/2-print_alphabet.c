eefcf1473fa484486a60f31e0c60f745bcc1bde5
Remenyr <puertaalejo02@gmail.com>
2022-10-14 10:25:45 -0500

    alphabet in order

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - return the alphabet
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		putchar (a);
+	}
+		putchar ('\n');
+	return (0);
+}
