31f75bde0fc5bfd123e94b0990ca3b9603598b07
Remenyr <puertaalejo02@gmail.com>
2022-10-14 11:33:56 -0500

    prints the alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *print_alphabet - it prints the alphabet
+ *Return: 0
+ */
+
+void print_alphabet(void)
+{
+	int a = 97;
+
+	while (a <= 122)
+	{
+		_putchar(a);
+		a++;
+
+	}
+	_putchar('\n');
+}
