c1e1c4d9ec3b73eb482a3c607b41975d2bd0b879
Remenyr <puertaalejo02@gmail.com>
2022-10-28 17:48:54 -0500

    all

---
+++ static_libraries/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * *_strcat - concatenates two strings
+ * @dest: destination pointer
+ * @src: origin pointer
+ * Return: dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	const char *src_ptr;
+	char *dest_ptr;
+
+	for (dest_ptr = dest; *dest_ptr != '\0'; dest_ptr++)
+		;
+
+	for (src_ptr = src; *src_ptr != '\0'; src_ptr++, dest_ptr++)
+	{
+		*dest_ptr = *src_ptr;
+	}
+
+	*dest_ptr = '\0';
+	return (dest);
+}
---
+++ static_libraries/100-atoi.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * @n - is a variable
+ *
+ * Return: 0;
+ */
+
+int main ()
+{
+
+
+}
